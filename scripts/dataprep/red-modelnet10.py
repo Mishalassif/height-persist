beb660152ec2ac79394c86910891b242e20fc507
Mishal Assif <msl.asf97@gmail.com>
2022-02-22 23:59:20 -0600

    dataprep scripts updated

--- scripts/dataprep/red-modelnet10.py
+++ scripts/dataprep/red-modelnet10.py
@@ -1,25 +1,27 @@
 import sys
 import os
-from core.off_utils import *
+from core.obj_utils import *
+
+#obj_class = sys.argv[1]
 
 manifold_path = './manifold'
 simplify_path = './simplify'
 
-root_dir = '../../datasets/ModelNet10/bathtub/test'
+#root_dir = '../../datasets/ModelNet40_obj/'+obj_class
+root_dir = '../../datasets/Unreduced_ModelNet40'
 tmp_file = 'tmp.obj'
 tmp2_file = 'tmp2.obj'
-list_off = get_all_off(root_dir)
+list_obj = get_all_obj(root_dir)
 
-print(list_off)
+#print(list_off)
 
 num_faces = 2000
 
 count = 0.0
-size = len(list_off)
-for off_file in list_off:
+size = len(list_obj)
+for obj_file in list_obj:
     count = count+1.0
-    os.system('meshlabserver -i ' + off_file + '.off' + ' -o ' + tmp_file)
-    os.system(simplify_path + ' -i ' + tmp_file + ' -o ' + tmp2_file  + ' -m -f ' + str(num_faces))
-    os.system(manifold_path + ' ' + tmp2_file + ' ' + off_file + '.obj')
-    os.system(simplify_path + ' -i ' + off_file + '.obj' + ' -o ' + off_file + '.obj' + ' -m -f ' + str(num_faces))
-    print("Done with " + str(round(float(count)/size, 2)*100) + "%")
+    #print(simplify_path + ' -i ' + obj_file + '.obj'  + ' -o ' + obj_file + '_red.obj'  + ' -m -f ' + str(num_faces))
+    os.system('timeout -s SIGINT 30m ' + manifold_path + ' ' + obj_file + '.obj'  + ' ' + obj_file + '_tmp.obj')
+    os.system('timeout -s SIGINT 30m ' + simplify_path + ' -i ' + obj_file + '_tmp.obj'  + ' -o ' + obj_file + '_red.obj'  + ' -m -f ' + str(num_faces))
+    os. system("echo \" Done with " + str(round(float(count)/size, 2)*100) + "% \" ")
